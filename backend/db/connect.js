3029e7c1993811ce6011ea1bc2aa3bc65220793e
AmritSonu <sonus87464@gmail.com>
2024-02-25 12:04:54 +0530

    setup for database

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,15 +1,14 @@
 import mongoose from "mongoose";
+const URL =
+  "mongodb+srv://nainamanchanda9:xIIhyjybHDBuYfx8@cluster0.gd6ejsq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-uri ="mongodb+srv://nimmo:lyHLDfYzADbWpqVv@cluster0.ufw3nnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-const connectDB = () =>{
-    return mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-};//Su9gsaMSTFmDmQat
-
-export {connectDB};
-//mongodb+srv://<username>:<password>@cluster0.ufw3nnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
-//mongodb+srv://<username>:<password>@cluster0.ufw3nnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+export { connectDB };
